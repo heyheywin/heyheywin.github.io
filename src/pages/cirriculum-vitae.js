b75a2a6df5ca24093770841259250490afc0aebd
Winfield Fan <faefaehk@gmail.com>
2020-04-22 19:12:06 +0800

    image fix

--- src/pages/cirriculum-vitae.js
+++ src/pages/cirriculum-vitae.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import CirVit from "../images/winfield-fan-cv"
+import CirVit from "../images/winfield-fan-cv.jpg"
 
 
 const cv = () => (
